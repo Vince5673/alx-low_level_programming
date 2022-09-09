68d9de5e2d16dde9e88fe2ab83af1fba126b2c76
Vince5673 <nkwochavincent5673@gmail.com>
2022-09-09 08:32:35 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,19 @@
-root@56ef13b8d982.12890
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Description:prints the alphabet
+ *
+ *Return: Always 0 (successful)
+ */
+int main(void)
+{
+	int letter, n;
+
+	for (letter = 97, n = 1 ; n < 27; letter++, n++)
+	{
+		putchar(letter);
+	}
+	putchar(10);
+	return (0);
+}
