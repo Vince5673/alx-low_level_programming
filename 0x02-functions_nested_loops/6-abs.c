b9de037e17e29e998249623a2df37aa6072a39e6
Vince5673 <nkwochavincent5673@gmail.com>
2022-09-13 13:52:28 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,26 +1,24 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * returns an absolute value of an int
- *
- * @n: this file provides the values needed
- *
- * Return: 0;
+ * _abs - Entry point
+ * @n: n is an integer
+ * Description: prints the absolute value of an integer
+ * Return: int
  */
 
 int _abs(int n)
-
 {
-
-	if (n < 0)
-
+	if (n > 0)
 	{
-
-		return (n * -1);
-
+		return (n);
 	}
-
-	return (n);
-
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
 }
