e1c9f098c54acc869756994416f5ebc737e31346
V I N C E <108697159+Vince5673@users.noreply.github.com>
2022-10-04 23:05:44 +0100

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,39 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 /**
- *create_array - array for prints a string
- *@size: number elements array
- *@c: char
- *Return: pointer
+ *create_array - creates array of chars initializes it to a specific char
+ *@size:intended size
+ *@c:character
+ *
+ *Return:Null,pointer to array
  */
-
 char *create_array(unsigned int size, char c)
 {
-char *buffer;
-unsigned int position;
+	unsigned int i;
+	char *p;
 
-if (size == 0)
-{
-return (NULL);
-}
-
-/*Define values with malloc*/
-buffer = (char *) malloc(size * sizeof(c));
-
-if (buffer == 0)
-{
-return (NULL);
-}
-
-else
-{
-position = 0;
-while (position < size) /*While for array*/
-{
-*(buffer + position) = c;
-position++;
-}
-
-return (buffer);
-}
-
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	p = malloc(sizeof(*p) * size);
+	if (!p)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		*(p + i) = c;
+	}
+	return (p);
 }
