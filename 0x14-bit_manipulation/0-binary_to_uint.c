a313eb740947acb995467d661a2af666c0ea04f7
V I N C E <108697159+Vince5673@users.noreply.github.com>
2022-10-28 06:22:31 +0100

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,33 @@
 #include "main.h"
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 /**
- * binary_to_uint - converts a binary number to an
- * unsigned int.
- * @b: binary.
- *
- * Return: unsigned int.
+ *binary_to_uint - converts a binary number to an unsigned int
+ *@b:pointer to a string containing 1 and 0
+ *Return:converted number or 0,otherwise
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui;
-	int len, base_two;
+	int i;
+	unsigned int n = 0;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
-
-	ui = 0;
-
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	i = 0;
+	while (b[i])
 	{
-		if (b[len] != '0' && b[len] != '1')
-		{
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		}
-
-		if (b[len] & 1)
+		i++;
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		n = n << 1;
+		if (b[i] == '1')
 		{
-			ui += base_two;
+			n += 1;
 		}
 	}
-
-	return (ui);
+	return (n);
 }
